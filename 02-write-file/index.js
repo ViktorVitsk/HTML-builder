c2891694f987afeb69a5c09580f97177d6fbda25
Viktor Stoianov <viktor.web.frontend@gmail.com>
2022-05-22 20:58:52 +0300

    feat: add solve second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline/promises');
+const { stdin: input, stdout: output } = require('process');
+
+const rl = readline.createInterface({ input, output });
+
+const writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+rl.write('Введите текст для записи: \n');
+
+rl.on('line', (txt) => {
+  if (txt.trim() === 'exit' || txt.includes(' exit')) {
+    console.log('\nПрограмма закрыта');
+    writeText(txt.replace('exit', ''));
+    rl.close();
+  } else {
+    writeText(txt);
+  }
+  
+});
+
+rl.on('SIGINT', () => {
+  rl.close();
+  console.log('\nПрограмма закрыта');
+});
+
+function writeText(txt) {
+  writeStream.write(`${txt}\n`, (err) => {
+    if (err) throw err;
+  });
+}
