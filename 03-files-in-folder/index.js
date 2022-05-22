d42c121e31158c29a1c0b38a06a107ec41ceb6bb
Viktor Stoianov <viktor.web.frontend@gmail.com>
2022-05-22 22:19:01 +0300

    feat: add solve third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,34 @@
+const { readdir } = require('fs/promises');
+const path = require('path');
+const fs = require('fs');
+
+showFiles(path.join(__dirname, 'secret-folder'));
+
+async function showFiles(p) {
+  try {
+    const files = await readdir(p, 'utf-8', true);
+
+    for (const file of files) {
+      const currentPath = path.join(p, file);
+
+      fs.stat(currentPath, (err, stats) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        if (stats.isFile()) {
+          console.log(
+            `name: ${
+              path.basename(currentPath).split('.')[0]
+            }\textname: ${path.extname(currentPath)}\tsize: ${(stats.size/1024).toFixed(3)} kb`
+          );
+        } 
+        // else if (stats.isDirectory()) {
+        //   showFiles(currentPath);
+        // }
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
